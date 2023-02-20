09f0d26a613cf175fc98b768ae7aad4daa89303c
Tarikkoc <ahmettarikkoc@gmail.com>
2023-02-20 19:58:20 +0300

    tarikkoc

--- src/Likes/Header.js
+++ src/Likes/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import Back from "./img/backSvg.svg";
 
 const Header = () => {
+  const history = useHistory();
+  const handleClick = () => {
+    history.goBack();
+  };
   return (
     <div className="flex pt-6">
-      <img className="w-8 h-8 cursor-pointer" src={Back} alt="" />
+      <img
+        onClick={handleClick}
+        className="w-8 h-8 cursor-pointer"
+        src={Back}
+        alt=""
+      />
       <span className="w-[80%] pl-4 font-medium text-2xl">Bildirimler</span>
     </div>
   );
